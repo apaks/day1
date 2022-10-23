fc46d2637e85c92ebdf07e1a2bf15956e9e0fc70
apaks <18731254+apaks@users.noreply.github.com>
2022-10-23 03:13:06 -0400

    trail

--- src/js/trail.js
+++ src/js/trail.js
@@ -1,7 +1,7 @@
 // Get the texture for rope.
 import * as PIXI from 'pixi.js'
 
-const trailTexture = PIXI.Texture.from('/assets/trail.png');
+const trailTexture = PIXI.Texture.from('/assets/bunny.png');
 const historyX = [];
 const historyY = [];
 // historySize determines how long the trail will be.
