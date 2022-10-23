753b0e4b20c0cdec5fc2700ddc16fdece0059c45
apaks <18731254+apaks@users.noreply.github.com>
2022-10-23 01:31:58 -0400

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,8 @@
 const {merge} = require('webpack-merge')
 const common = require('./webpack.common.js')
+
 module.exports = merge(common, {
-  'mode':'production',
+  mode:'production',
   devtool: 'source-map',
   module: {
     rules: [{
